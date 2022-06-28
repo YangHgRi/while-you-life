4a2a2724b3f8de67dcf84818effb043aedac2e1e
YangHgRi <57426791+YangHgRi@users.noreply.github.com>
2022-06-28 23:45:09 +0800

    初始化依赖

---
+++ while-you-play/src/main/java/yanghgri/whileyouplay/WhileYouPlayApplication.java
@@ -0,0 +1,11 @@
+package yanghgri.whileyouplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhileYouPlayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WhileYouPlayApplication.class, args);
+    }
+}
