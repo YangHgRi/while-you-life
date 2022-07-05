5cdbe74a7cc9bccbe9e6827f68fe24da596f89e2
YangHgRi <57426791+YangHgRi@users.noreply.github.com>
2022-07-05 16:33:00 +0800

    异步执行定时任务

--- while-you-play/src/main/java/yanghgri/whileyouplay/task/LogTask.java
+++ while-you-play/src/main/java/yanghgri/whileyouplay/task/LogTask.java
@@ -1,6 +1,7 @@
 package yanghgri.whileyouplay.task;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import java.time.format.DateTimeFormatter;
 public class LogTask {
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+    @Async
     @Scheduled(cron = "0/10 * * * * *")
     public void logCurrentTime() {
         log.info(dateTimeFormatter.format(LocalDateTime.now()));
