916c611feb19ce2c914a65de0d5af81b0f227c40
YangHgRi <57426791+YangHgRi@users.noreply.github.com>
2022-07-05 16:32:19 +0800

    编写定时任务: 日志报时

---
+++ while-you-play/src/main/java/yanghgri/whileyouplay/channel/DiscardChannel.java
@@ -0,0 +1,5 @@
+package yanghgri.whileyouplay.channel;
+
+public class DiscardChannel {
+
+}
---
+++ while-you-play/src/main/java/yanghgri/whileyouplay/task/LogTask.java
@@ -0,0 +1,19 @@
+package yanghgri.whileyouplay.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@Component
+public class LogTask {
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Scheduled(cron = "0/10 * * * * *")
+    public void logCurrentTime() {
+        log.info(dateTimeFormatter.format(LocalDateTime.now()));
+    }
+}
